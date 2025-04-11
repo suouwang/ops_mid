7594484c3ae8de0262b40c2d226d1427899506a7
suouwang <138356121+suouwang@users.noreply.github.com>
2025-04-11 22:17:15 +0800

    Update static.py

--- static.py
+++ static.py
@@ -7,13 +7,12 @@ response = rq.get(url)
 soup = BeautifulSoup(response.text,"lxml")
 #print(soup.prettify())
 
-def readurl(url,f,f2,jFile,count):
+def readurl(url,jFile,count):
     if count == 3:
         return None
     response = rq.get(url)
     soup = BeautifulSoup(response.text,"lxml")
     #print(soup.prettify())
-    f2.write(soup.prettify())
     for inf in soup.find_all("div",class_="title"):
         a = inf.find('a')
         if a != None:
@@ -22,17 +21,11 @@ def readurl(url,f,f2,jFile,count):
                 json_ori = {'title':r,
                             'url':'https://www.ptt.cc'+a['href']}
                 json.dump(json_ori,jFile,indent=2,ensure_ascii=False)
-                f.write(a.text+'\n')
-                f.write('https://www.ptt.cc'+a['href']+'\n\n')
     ##
     for btn in soup.find_all('a',class_="btn wide"):
         if btn.text == '‹ 上頁':
-            readurl('https://www.ptt.cc'+btn['href'],f,f2,jFile,count+1)
-
-
-
-f2 = open('html.txt','w', encoding='utf-8')
-f = open('text.txt','w', encoding='utf-8')
+            readurl('https://www.ptt.cc'+btn['href'],jFile,count+1)
+            
 jFile = open('file.json','w',encoding="utf-8")
 count = 0
-readurl(url,f,f2,jFile,count)
+readurl(url,jFile,count)
