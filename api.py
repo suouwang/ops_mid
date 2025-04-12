50abf7050e2164991a6af02a20c48db5c41ad257
suouwang <138356121+suouwang@users.noreply.github.com>
2025-04-12 22:41:42 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.edge.options import Options
 import time
 import csv
 
-driver = webdriver.Edge()
+options = Options()
+options.add_argument('--headless')  # 無頭模式
+options.add_argument('--disable-gpu')
+
+driver = webdriver.Edge(options=options)
 driver.implicitly_wait(2)
 driver.get("https://www.coolpc.com.tw/evaluate.php")
 action = ActionChains(driver)
@@ -17,8 +22,6 @@ def collector(driver,name,writer):
     a = driver.find_element(By.NAME,name)
     #print(a.text)
     for r in a.text.split('\n'):
-        #if r.startswith('共有商品'):
-        #    continue
         a = r.split(', ')
         if len(a) == 2:
             writer.writerow([a[0],a[1]])
